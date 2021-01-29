d38afacc8b2683cc02821ea0f55846da101292f2
Nathália <nathaliaveneziano@icloud.com>
2021-01-29 19:18:12 -0300

    Obtendo os dados do fetch e passando para nova funçao

--- scripts.js
+++ scripts.js
@@ -1,8 +1,33 @@
+const handleRates = (ratesData) => {
+
+};
+
+const fetchCurrency = (currency) => {
+  const endpoint = `https://api.ratesapi.io/api/latest?base=${currency}`;
+
+  fetch(endpoint)
+    .then((response) => response.json())
+    .then((object) => {
+      if (object.error) {
+        // caso o objeto retorne a chave ERROR, jogamos para o catch tratar
+        // erro na regra de negócio
+        throw new Error(object.error);
+      }
+
+      handleRates(object);
+
+    })
+    .catch((error) => {
+      // esse bloco trata erros das requisições acima ou quando levantados
+      window.alert(error);
+    });
+};
+
 const handleSearchEvent =() => {
   const searchInput = document.querySelector('#currency-input');
   const currency = searchInput.value.toUpperCase();
 
-  console.log('button click', currency);
+  fetchCurrency(currency);
 };
 
 const setupEvents = () => {
