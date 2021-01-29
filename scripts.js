706eb4bb9221a4ac638e5b6110b1ce9cdf518ace
Nathália <nathaliaveneziano@icloud.com>
2021-01-29 16:27:13 -0300

    Button AddEventListener

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-console.log('teste');
+const handleSearchEvent =() => {
+  const searchInput = document.querySelector('#currency-input');
+  const currency = searchInput.value;
+  console.log('button click', currency);
+};
+
+const setupEvents = () => {
+  const searchButton = document.querySelector('#search-button');
+  searchButton.addEventListener('click', handleSearchEvent);
+}
+
+window.onload = () => {
+  setupEvents();
+
+}
+
+// listar branchs remotas = git banch -a
+// puxar branch remota = git checkout nome-branch
